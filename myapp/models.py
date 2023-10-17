9c812e6286f83212211f7ddaefadf46b739931d3
Sadiar_Mohammadi <sadiarmohammadi@yahoo.com>
2023-10-17 20:38:43 +0330

    add verbose name to model

--- myapp/models.py
+++ myapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Customer(models.Model):
-    username = models.CharField(max_length=20)
+    username = models.CharField(max_length=20,verbose_name='نام کاربری')
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=30)
     mobile = models.CharField(max_length=11)
