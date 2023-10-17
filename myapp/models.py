cd6ba61d0fc5abeb72199b752ea83ce6faf5d322
javda_jahangiri <javad.jahangiri.niopdc@gamil.com>
2023-10-17 20:53:23 +0330

    add verbose name to model customer

--- myapp/models.py
+++ myapp/models.py
@@ -4,15 +4,19 @@ from django.db import models
 # Create your models here.
 
 class Customer(models.Model):
-    username = models.CharField(max_length=20,verbose_name='نام کاربری')
-    first_name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length=30)
-    mobile = models.CharField(max_length=11)
-    email = models.EmailField(max_length=50)
-    birthday = models.DateTimeField(null=True, blank=True)
+    username = models.CharField(max_length=20, verbose_name='نام کاربری')
+    first_name = models.CharField(max_length=25, verbose_name='نام ')
+    last_name = models.CharField(max_length=30, verbose_name='نام خانوادگی')
+    mobile = models.CharField(max_length=11, verbose_name='موبایل')
+    email = models.EmailField(max_length=50, verbose_name='ایمیل')
+    birthday = models.DateTimeField(null=True, blank=True, verbose_name='تاریخ تولد')
 
     def __str__(self):
         return f'{self.first_name}--{self.last_name}'
+
+    class Meta:
+        verbose_name = 'مشتری'
+        verbose_name_plural = 'مشتریان'
 
 
 class Product(models.Model):
